1b5fba653838e07c5c45fc945a3067b4cf12f4ee
zapashnii <Aleksandr.Zapashniy@gorparkovki.ru>
2024-07-25 16:55:20 +0300

    refactor

--- googleplay/src/main/java/ru/fabit/klyanchilka/googleplay/GoogleApplicationRateMe.kt
+++ googleplay/src/main/java/ru/fabit/klyanchilka/googleplay/GoogleApplicationRateMe.kt
@@ -2,6 +2,8 @@ package ru.fabit.klyanchilka.googleplay
 
 import android.app.Activity
 import android.content.Context
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Status.*
 import com.google.android.play.core.review.ReviewManagerFactory
 import ru.fabit.klyanchilka.core.ApplicationRateMe
 import ru.fabit.klyanchilka.core.RateMeInfoCallBack
@@ -11,11 +13,17 @@ open class GoogleApplicationRateMe(context: Context) : ApplicationRateMe {
     private val manager = ReviewManagerFactory.create(context)
 
     override fun rateMe(activity: Any, rateMeInfoCallBack: RateMeInfoCallBack) {
+        val timeStart = System.currentTimeMillis()
         manager.requestReviewFlow()
             .addOnSuccessListener { reviewInfo ->
                 manager.launchReviewFlow(activity as Activity, reviewInfo)
                     .addOnSuccessListener {
-                        rateMeInfoCallBack.onSuccess()
+                        val timeEnd = System.currentTimeMillis()
+                        if (timeEnd - timeStart >= 300) {
+                            rateMeInfoCallBack.onSuccess()
+                        } else {
+                            rateMeInfoCallBack.onFailure(ApiException(RESULT_CANCELED))
+                        }
                     }
                     .addOnFailureListener { throwable ->
                         rateMeInfoCallBack.onFailure(throwable)
